8d5f85190d8f6a6fdc46e6a8ca52a5159df2172e
Alessandro Franceschi <al@lab42.it>
2012-01-12 14:56:59 +0100

    Added test to check for OldGen modules compatibility

--- spec/classes/splunk_spec.rb
+++ spec/classes/splunk_spec.rb
@@ -140,4 +140,21 @@ describe 'splunk' do
       content.should == "iptables"
     end
   end
+
+  describe 'Test OldGen Module Set Integration' do
+    let(:params) { {:monitor => "yes" , :monitor_tool => "puppi" , :firewall => "yes" , :firewall_tool => "iptables" , :puppi => "yes" , :port => "42" } }
+
+    it 'should generate monitor resources' do
+      content = catalogue.resource('monitor::process', 'splunk_process').send(:parameters)[:tool]
+      content.should == "puppi"
+    end
+    it 'should generate firewall resources' do
+      content = catalogue.resource('firewall', 'splunk_tcp_42').send(:parameters)[:tool]
+      content.should == "iptables"
+    end
+    it 'should generate puppi resources ' do 
+      should contain_file('puppi_splunk').with_ensure('present') 
+    end
+  end
+
 end
